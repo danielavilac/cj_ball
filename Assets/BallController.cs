a095cb2e14de4825e9e4fa139c7f507ac6b48b72
danielavilac <lad.daniel.avila@gmail.com>
2015-05-05 15:43:52 -0500

    Initial commit

---
+++ Assets/BallController.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BallController : MonoBehaviour
+{
+    public PlatformsController platformsController;
+
+    void Awake ()
+    {
+        platformsController = Camera.main.GetComponent<PlatformsController>();
+        SetInitialColor();
+    }
+
+    void OnCollisionEnter (Collision col)
+    {
+        Color ballColor = gameObject.renderer.material.color;
+        Color platformColor = col.gameObject.renderer.material.color;
+        if (ballColor == platformColor)
+        {
+            Debug.Log("Same color");
+        } 
+        else
+        {
+            Debug.Log("Different color");
+        }
+        SetColor();
+    }
+
+    private void SetColor ()
+    {
+        int index = Random.Range(0, platformsController.colors.Length);
+        gameObject.renderer.material.color = platformsController.colors[index];
+    }
+
+    private void SetInitialColor ()
+    {
+        // Initial color has to be always the same color of the center platform
+        gameObject.renderer.material.color = platformsController.colors[0];
+    }
+}
---
+++ Assets/PlatformsController.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class PlatformsController : MonoBehaviour
+{
+    public List<GameObject> platforms;
+    public Color[] colors = new Color[5] {Color.red, Color.blue, Color.green, Color.yellow, Color.white};
+
+    void Awake ()
+    {
+        ShuffleArray(colors);
+    }
+
+    void Start ()
+    {
+        for (int i = 0; i < platforms.Count; i++) 
+        {
+            platforms[i].renderer.material.color = colors[i];
+        }
+    }
+	
+	void Update ()
+    {
+        foreach (GameObject platform in platforms)
+        {
+            platform.transform.Translate(Input.acceleration.x, 0, 0);
+        }
+	}
+
+    public void ShuffleArray<T>(T[] arr) {
+        for (int i = arr.Length - 1; i > 0; i--) {
+            int r = Random.Range(0, i);
+            T tmp = arr[i];
+            arr[i] = arr[r];
+            arr[r] = tmp;
+        }
+    }
+}
